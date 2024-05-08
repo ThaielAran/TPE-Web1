c3f257a625b5fa1c9db751eb9f5a760cfcda99b6
Thaiel Aran Gallardo <thaiel.a.gallarco@gmail.com>
2024-05-08 17:29:54 -0300

    dark mode

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -6,3 +6,15 @@ function toggleMenu(){
     document.querySelector(".navigation").classList.toggle("show");
 }
 
+document.querySelector("#darkMode").addEventListener("click", darkMode);
+function darkMode(){
+ document.querySelector("#fondo").classList.toggle("darkBack")
+ document.querySelector(".menu").classList.toggle("darkMenu")
+ document.querySelector("h1").classList.toggle("darkLetters")
+ document.querySelector(".aboutUs").classList.toggle("darkLetters")
+/*NO SE APLICAN*/
+ document.querySelector(".content").classList.add("darkLetters")
+ document.querySelector("#menuBtn").src="images/menuBtnD.png"
+ document.querySelector("#icon").src="images/LogoD.png"
+ 
+}
