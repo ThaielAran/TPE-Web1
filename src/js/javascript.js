c0adcfa57512a193b8fc8852dec1b1bb089a008d
Vespetxe <pekaespetxe@gmail.com>
2024-05-10 18:10:30 -0300

    Update javascript.js

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,25 +1,14 @@
 document.querySelector('#btn-menu').addEventListener("click", toggleMenu);
 document.querySelector("#darkMode").addEventListener("click", darkMode);
 
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("show");
+}
+
 let captchArreglo = ['jk5lf','h1y4s','d3toa','rg7eb']
 let indice=(Math.floor(Math.random() * 4) + 0);
 document.querySelector("#captchaImg").src="images/captcha" + indice + ".png";
 document.querySelector("#sbmtBtn").addEventListener("click", captchaCheck) 
-
-function toggleMenu() {
-
-    document.querySelector(".navigation").classList.toggle("show");
-}
-
-function darkMode() {
-    document.querySelector("#icon").classList.toggle("darkImg")
-    document.querySelector("#menuBtn").classList.toggle("darkImg")
-    document.querySelector("#fondo").classList.toggle("darkBack")
-    document.querySelector(".menu").classList.toggle("darkMenu")
-    document.querySelector("h1").classList.toggle("darkLetters")
-    document.querySelector("main").classList.toggle("darkLetters")
-    
-}
 
 function captchaCheck(){
     let flag= false;
@@ -33,9 +22,22 @@ function captchaCheck(){
     document.querySelector(".captcha").appendChild(message)
     if(flag){
         message.innerHTML="Message has been sent successfully!"
-        message.classList.add("msgBueno")
+        message.classList.add("msgOk")
     }else{
         message.innerHTML="Message could not be sent. Check captcha input."
-        message.classList.add("msgMalo")
+        message.classList.add("msgWrong")
     }
 }
+
+
+function darkMode() {
+    document.querySelector("#icon").classList.toggle("darkImg")
+    document.querySelector("#menuBtn").classList.toggle("darkImg")
+    document.querySelector("#fondo").classList.toggle("darkBack")
+    document.querySelector(".menu").classList.toggle("darkMenu")
+    document.querySelector("h1").classList.toggle("darkLetters")
+    document.querySelector("main").classList.toggle("darkLetters")
+    
+}
+
+
