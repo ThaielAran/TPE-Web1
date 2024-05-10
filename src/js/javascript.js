58c83ce129fb0d7beb96e77ee4e88dbfc6ca8eba
Thaiel Aran Gallardo <thaiel.a.gallarco@gmail.com>
2024-05-10 13:02:16 -0300

    añadido formulario, pulido dark mode

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,20 +1,18 @@
 document.querySelector('#btn-menu').addEventListener("click", toggleMenu);
+document.querySelector("#darkMode").addEventListener("click", darkMode);
 
-
-function toggleMenu(){
+function toggleMenu() {
 
     document.querySelector(".navigation").classList.toggle("show");
 }
 
-document.querySelector("#darkMode").addEventListener("click", darkMode);
-function darkMode(){
- document.querySelector("#fondo").classList.toggle("darkBack")
- document.querySelector(".menu").classList.toggle("darkMenu")
- document.querySelector("h1").classList.toggle("darkLetters")
- document.querySelector(".aboutUs").classList.toggle("darkLetters")
-/*NO SE APLICAN*/
- document.querySelector(".content").classList.add("darkLetters")
- document.querySelector("#menuBtn").src="images/menuBtnD.png"
- document.querySelector("#icon").src="images/LogoD.png"
- 
+function darkMode() {
+    document.querySelector("#icon").src = "images/LogoD.jpg"
+    document.querySelector("#menuBtn").src = "images/menuBtnD.png"
+    document.querySelector("#fondo").classList.toggle("darkBack")
+    document.querySelector(".menu").classList.toggle("darkMenu")
+    document.querySelector("h1").classList.toggle("darkLetters")
+    document.querySelector("main").classList.toggle("darkLetters")
+    
 }
+
