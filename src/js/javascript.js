9a1923087adf6caf19cda292063faebc93896d11
Thaiel Aran Gallardo <thaiel.a.gallarco@gmail.com>
2024-05-10 15:58:58 -0300

    added captcha

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,6 +1,11 @@
 document.querySelector('#btn-menu').addEventListener("click", toggleMenu);
 document.querySelector("#darkMode").addEventListener("click", darkMode);
 
+let captchArreglo = ['jk5lf','h1y4s','d3toa','rg7eb']
+let indice=(Math.floor(Math.random() * 4) + 0);
+document.querySelector("#captchaImg").src="images/captcha" + indice + ".png";
+document.querySelector("#sbmtBtn").addEventListener("click", captchaCheck) 
+
 function toggleMenu() {
 
     document.querySelector(".navigation").classList.toggle("show");
@@ -16,3 +21,21 @@ function darkMode() {
     
 }
 
+function captchaCheck(){
+    let flag= false;
+    let captchaInput=document.querySelector("#inputCaptcha").value.toLowerCase()
+    if(captchaInput == captchArreglo[indice])
+            flag=true;
+        else
+            flag=false
+
+    let message = document.createElement("div");
+    document.querySelector(".captcha").appendChild(message)
+    if(flag){
+        message.innerHTML="Message has been sent successfully!"
+        message.classList.add("msgBueno")
+    }else{
+        message.innerHTML="Message could not be sent. Check captcha input."
+        message.classList.add("msgMalo")
+    }
+}
