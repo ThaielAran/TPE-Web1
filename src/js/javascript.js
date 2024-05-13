63d73a9cc7f72bbf6a7d0c3cf83851c813872b62
Vespetxe <pekaespetxe@gmail.com>
2024-05-13 12:57:06 -0300

    Update javascript.js

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -8,35 +8,35 @@ function toggleMenu() {
 let captchArreglo = ['jk5lf','h1y4s','d3toa','rg7eb']
 let indice=(Math.floor(Math.random() * 4) + 0);
 document.querySelector("#captchaImg").src="images/captcha" + indice + ".png";
-document.querySelector("#sbmtBtn").addEventListener("click", captchaCheck) 
+document.querySelector("#sbmtBtn").addEventListener("click", captchaCheck);
 
 function captchaCheck(){
     let flag= false;
-    let captchaInput=document.querySelector("#inputCaptcha").value.toLowerCase()
+    let captchaInput=document.querySelector("#inputCaptcha").value.toLowerCase();
     if(captchaInput == captchArreglo[indice])
             flag=true;
         else
-            flag=false
+            flag=false;
 
     let message = document.createElement("div");
-    document.querySelector(".captcha").appendChild(message)
+    document.querySelector(".captcha").appendChild(message);
     if(flag){
-        message.innerHTML="Message has been sent successfully!"
-        message.classList.add("msgOk")
+        message.innerHTML="Message has been sent successfully!";
+        message.classList.add("msgOk");
     }else{
-        message.innerHTML="Message could not be sent. Check captcha input."
-        message.classList.add("msgWrong")
+        message.innerHTML="Message could not be sent. Check captcha input.";
+        message.classList.add("msgWrong");
     }
 }
 
 
 function darkMode() {
-    document.querySelector("#icon").classList.toggle("darkImg")
-    document.querySelector("#menuBtn").classList.toggle("darkImg")
-    document.querySelector("#fondo").classList.toggle("darkBack")
-    document.querySelector(".menu").classList.toggle("darkMenu")
-    document.querySelector("h1").classList.toggle("darkLetters")
-    document.querySelector("main").classList.toggle("darkLetters")
+    document.querySelector("#icon").classList.toggle("darkImg");
+    document.querySelector("#menuBtn").classList.toggle("darkImg");
+    document.querySelector("#fondo").classList.toggle("darkBack");
+    document.querySelector(".menu").classList.toggle("darkMenu");
+    document.querySelector("h1").classList.toggle("darkLetters");
+    document.querySelector("main").classList.toggle("darkLetters");
     
 }
 
